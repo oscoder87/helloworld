5fc4857043b9ec5a3532b4b2df16b13a22a27416
Antonio Elrio <antoelca@gmail.com>
2022-04-20 18:29:42 +0200

    Importacion de org.apache.commons lang3 realizada a través del POM

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,9 +1,13 @@
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * Created by jt on 2018-11-26.
+ * Modified by Oscoder on 2022-04-19
  */
 public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World!!!! ");
+        System.out.println(StringUtils.upperCase("texto en minuscula en el codigo 12345 que debe Imprimirse\npor pantalla en mayuscula"));
     }
 }
