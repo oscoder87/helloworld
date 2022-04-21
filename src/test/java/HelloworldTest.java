9afcc0fd3709371265c49b3dc752616e8307351a
Antonio Elrio <antoelca@gmail.com>
2022-04-21 18:05:20 +0200

    Ejecutar tests.

---
+++ src/test/java/HelloworldTest.java
@@ -0,0 +1,9 @@
+public class HelloworldTest {
+    public void testHello(){
+        System.out.println("Hola, desde el test. HELLO");
+    }
+
+    public void testWorld(){
+        System.out.println("Hola, desde el test. WORLD");
+    }
+}
